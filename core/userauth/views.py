3c80c4a02ffa3a29c92999f3f61f498acdc83f06
Aaryan <143906745+Aaryan-2k@users.noreply.github.com>
2025-07-07 15:43:35 +0000

    implemented messages for ease

--- core/userauth/views.py
+++ core/userauth/views.py
@@ -9,15 +9,17 @@ def register(request):
     if request.method=='POST':
         check=User.objects.filter(username=request.POST['username'])
         if check.exists():
-            messages.info(request, "username already taken")
+            messages.error(request, f'''The username "{request.POST['username']}" has already been taken.''')
             return redirect('/auth/register/')
         data={"username":request.POST['username'], "email":request.POST['email']}
         print(data)
         user=User(**data)
         user.set_password(request.POST['password'])
         user.save()
-        messages.success(request, "account created successfully")
+        messages.success(request, "Your account has been created successfully!")
         return redirect('/auth/login/')
+    if request.user.is_authenticated:
+        return redirect("/list/")
     return render(request,"register.html")
 
 def login_page(request):
@@ -25,16 +27,20 @@ def login_page(request):
         username=request.POST["username"]
         password=request.POST["password"]
         if not User.objects.filter(username=username).exists():
-            messages.warning(request, "invalid username or password")
+            messages.warning(request, "Invalid Credentials!")
             return redirect('/auth/login/')
         user=authenticate(username=username, password=password)
         if user==None:
-            messages.warning(request, "invalid username or password")
+            messages.warning(request, "Invalid Credentials!")
             return redirect('/auth/login/')
         login(request, user)
+        messages.success(request,f'''Welcome back {user.username}''')
+        return redirect("/list/")
+    if request.user.is_authenticated:
         return redirect("/list/")
     return render(request,"login.html")
 
 def logout_page(request):
     logout(request)
+    messages.info(request, "You have been logged out!")
     return redirect('/auth/login/')
